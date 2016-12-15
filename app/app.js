f1ccec25ee13bc933f0e895561e28625325d48ad
luanatsainsburys <luan.au@sainsburys.co.uk>
2016-12-15 13:42:43 +0000

    Complete tutorial

--- app/app.js
+++ app/app.js
@@ -9,7 +9,12 @@ class App extends Component {
             <Route path='address' component={Address}>
                 <IndexRoute component={TwitterFeed} />
                 <Route path='instagram' component={Instagram} />
+                <Route path='query' component={Query} />
             </Route>
+            <Route path='/namedComponent' component={NamedComponents}>
+                <IndexRoute components={{ title: Title, subTitle: SubTitle }} />
+            </Route>
+            <Route path='/about(/:name)' component={About} />
             <Route path='*' component={NotFound} />
         </Route>
       </Router>
@@ -27,9 +32,17 @@ const Address = (props) => <div>
 const NotFound = () => (<h1>404.. This page is not found!</h1>)
 const Nav = () => (
   <div>
-    <IndexLink activeStyle={{color:'#53acff'}} to='/'>Home</IndexLink>&nbsp;
-    <IndexLink activeStyle={{color:'#53acff'}} to='/address'>Address</IndexLink>
-    <IndexLink activeStyle={{color:'#53acff'}} to='/about'>About</IndexLink>
+    <IndexLink activeClassName='active' to='/'>Home</IndexLink>&nbsp;
+    <IndexLink activeClassName='active' to='/address'>Address</IndexLink>&nbsp;
+    <IndexLink activeClassName='active' to='/about'>About</IndexLink>&nbsp;
+    <IndexLink activeClassName='active' to='/namedComponent'>Named Components</IndexLink>&nbsp;
+    <IndexLink 
+      activeClassName='active' 
+      to={{ 
+        pathname: '/address/query', 
+        query: { message: 'Hello from Route Query' } 
+      }}>Route Query
+  </IndexLink>
   </div>
 )
 const Container = (props) => <div>
@@ -38,5 +51,25 @@ const Container = (props) => <div>
 </div>
 const Instagram = () => <h3>Instagram Feed</h3>
 const TwitterFeed = () => <h3>Twitter Feed</h3>
-
+const NamedComponents = (props) => (
+  <div>
+    {props.title}<br />
+    {props.subTitle}
+  </div>
+)
+const Title = () => (
+  <h1>Hello from Title Component</h1>
+)
+const SubTitle = () => (
+  <h1>Hello from SubTitle Component</h1>
+)
+const About = (props) => (
+  <div>
+    <h3>Welcome to the About Page</h3>
+    {props.params.name && <h2>Hello, {props.params.name}</h2>}
+  </div>
+)
+const Query = (props) => (
+  <h2>{props.location.query.message}</h2>
+)
 export default App
